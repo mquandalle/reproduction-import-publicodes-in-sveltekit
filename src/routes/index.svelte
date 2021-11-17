1062008fce362d9747a9acbb38ed9b601efd98ef
Maxime Quandalle <maxime@quandalle.com>
2021-11-17 12:19:21 +0100

    repro

---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<script>
+  import Publicodes from "publicodes";
+
+  // Corretly imported on the server, but undefined on the client
+  console.log("publicodes", Publicodes);
+</script>
+
+<h1>Hello world</h1>
